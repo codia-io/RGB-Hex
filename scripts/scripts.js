f7d644b6fb8b360cd0423ec50815257080cfaa7f
username <email>
2020-10-07 18:53:37 +0100

    things are good i guess

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,44 @@
 // navbar and showcase scroll effect
 const checkpoint = 200;
+const lg_breakpoint = 980;
+
+function top_effect() {
+    $("header").removeClass("scroll-effect-on");
+    $("#img-logo").removeClass("img-logo-on mt-2");
+    $("header>div").addClass("mt-3");
+    $("header").removeClass("border-bot");
+}
+
+function bottom_effect() {
+    $("header").addClass("scroll-effect-on");
+    $("#img-logo").addClass("img-logo-on mt-2");
+    $("header>div").removeClass("mt-3");
+    $("header").addClass("border-bot");
+
+}
 
 function menu_animation() {
+    width = $(window).width();
     var scroll = $(window).scrollTop();
-    if (scroll < checkpoint) {
-        // original scroll place
-        $("header").removeClass("scroll-effect-on");
-        $("#img-logo").removeClass("img-logo-on mt-2");
-        $("header>div").addClass("mt-3");
-        $("header").removeClass("border-bot");
-        opacity = 1 - scroll / checkpoint;
+    if (width >= lg_breakpoint) {
+        $("header").addClass("fixed-top");
+        if (scroll < checkpoint) {
+            // original scroll place
+            top_effect();
+            opacity = 1 - scroll / checkpoint;
+            document.querySelector("#main-text").style.opacity = opacity;
+        } else {
+            //when scrolled down
+            bottom_effect();
+            opacity = 0;
+            document.querySelector("#main-text").style.opacity = opacity;
+        }
     } else {
-        //when scrolled down
-        $("header").addClass("scroll-effect-on");
-        $("#img-logo").addClass("img-logo-on mt-2");
-        $("header>div").removeClass("mt-3");
-        $("header").addClass("border-bot");
-        opacity = 0;
+        bottom_effect();
+        $("header").removeClass("fixed-top");
     }
-    document.querySelector("#main-text").style.opacity = opacity;
+
 }
 $(window).scroll(menu_animation);
-menu_animation()
+$(window).resize(menu_animation);
+menu_animation();
